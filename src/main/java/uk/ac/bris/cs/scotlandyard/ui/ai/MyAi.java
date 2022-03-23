27c8802f9174907bd3dfb72e7e5d7974e58f3f3b
Josh Acton <jgacton@protonmail.com>
2022-03-23 11:46:40 +0000

    Implemented Dijkstra's algo for shortest path between two nodes on Scotland Yard board.

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -7,8 +7,9 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.annotation.Nonnull;
 
-
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Transport;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.Ai;
 import uk.ac.bris.cs.scotlandyard.model.Board;
@@ -46,8 +47,63 @@ public class MyAi implements Ai {
 					moves = ImmutableList.copyOf(moves.stream().filter(x -> !x.accept(getDestinationFinal).equals(location)).toList());
 				}
 			}
+			System.out.println(getShortestPath(board.getSetup().graph, 21, 77));
 		}
 		return moves.get(new Random().nextInt(moves.size()));
+	}
+
+	private int getShortestPath(ImmutableValueGraph<Integer, ImmutableSet<Transport>> graph, int source, int destination) {
+
+		Set<Integer> Q = new HashSet<>();
+		Integer[] dist = new Integer[199];
+		Integer[] prev = new Integer[199];
+
+		for(Integer N : graph.nodes()) {
+			dist[N-1] = 1000000;
+			prev[N-1] = null;
+			Q.add(N);
+		}
+
+		dist[source - 1] = 0;
+
+		while(!Q.isEmpty()) {
+
+			int minDist = 1000000;
+			int u = 0;
+			for(Integer N : Q) {
+				if(dist[N-1] < minDist) {
+					minDist = dist[N-1];
+					u = N;
+				}
+			}
+
+			Q.remove(u);
+
+			for(Integer v : graph.adjacentNodes(u)) {
+				if(Q.contains(v)) {
+					int edgeVal = 0;
+					if(!graph.edgeValue(u, v).orElseThrow().isEmpty()) {
+						edgeVal = 1;
+					}
+					int alt = dist[u-1] + edgeVal;
+					if(alt < dist[v-1]) {
+						dist[v-1] = alt;
+						prev[v-1] = u;
+					}
+				}
+			}
+		}
+
+		List<Integer> path = new ArrayList<>();
+		path.add(destination);
+		while(destination != source) {
+			path.add(prev[destination-1]);
+			destination = prev[destination-1];
+		}
+
+		Collections.reverse(path);
+
+		return(path.size() - 1);
 	}
 
 	/*private ImmutableValueGraph<Integer, Float> generateGameTree() {
