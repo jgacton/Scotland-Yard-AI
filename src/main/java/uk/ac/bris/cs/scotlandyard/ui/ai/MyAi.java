0f705acf8816f0d90203f2811a9b2e287d69121e
Josh Acton <jgacton@protonmail.com>
2022-03-23 10:56:46 +0000

    implemented simple heuristic - checks detective not present

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,5 +1,6 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -12,32 +13,60 @@ import uk.ac.bris.cs.scotlandyard.model.Ai;
 import uk.ac.bris.cs.scotlandyard.model.Board;
 import uk.ac.bris.cs.scotlandyard.model.Move;
 import com.google.common.graph.ImmutableValueGraph;
+import uk.ac.bris.cs.scotlandyard.model.*;
+import java.util.*;
 
 public class MyAi implements Ai {
-	private ImmutableValueGraph<Integer, Float> gameTree;
+	//private ImmutableValueGraph<Integer, Float> gameTree;
 
 	@Nonnull @Override public String name() { return "Bubble :)"; }
 
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
-		// returns a random move, replace with your own implementation
 		var moves = board.getAvailableMoves().asList();
-		return moves.get(new Random().nextInt(moves.size()));
+		if(moves.stream().anyMatch(x -> x.commencedBy().isMrX())) {
+			System.out.println("it is Mr X");
+			Move.Visitor<Integer> getDestinationFinal = new Move.FunctionalVisitor<>((x -> x.destination), (x -> x.destination2));
+			List<Move> goodMoves = new ArrayList<>();
+			for(Piece piece : board.getPlayers()) {
+				final int location;
+				if(piece.isDetective()) {
+					location = board.getDetectiveLocation((Piece.Detective) piece).orElseThrow();
+					goodMoves.addAll(moves.stream().filter(x -> !x.accept(getDestinationFinal).equals(location)).toList());
+				}
+			}
+			System.out.println(goodMoves);
+			return goodMoves.get(new Random().nextInt(goodMoves.size()));
+		} else {
+			return moves.get(new Random().nextInt(moves.size()));
+		}
 	}
 
-	private ImmutableValueGraph<Integer, Float> generateGameTree() {
+	/*private ImmutableValueGraph<Integer, Float> generateGameTree() {
 		return null;
 	}
 
 	private Float miniMax(Integer node, int depth, boolean maximisingPlayer) {
-		if(depth == 0 || node.) {
-
+		if(depth == 0 || !currentBoard.getWinner().isEmpty()) {
+			return getHeuristicValue();
 		}
 		return null;
 	}
 
-	private Float getHeuristicValue() {
+	private Float getHeuristicValue(Move move) {
+
+		Move.Visitor<Integer> getDestinationFinal = new Move.FunctionalVisitor<>((x -> x.destination), (x -> x.destination2));
+		int destinationFinal = move.accept(getDestinationFinal);
+
+		for(Piece player : this.currentBoard.getPlayers()) {
+			if(player.isDetective()) {
+
+			}
+		}
+
 		return null;
-	}
+	}*/
 }
+
+/* How deep to go in game tree, can figure out average number of connections of nodes on game board */
